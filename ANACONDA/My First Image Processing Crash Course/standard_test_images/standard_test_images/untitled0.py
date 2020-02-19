e047715393d1f9cd993538a8f8e9fc64ca498035
Kaustav Ghosh <teetangh@gmail.com>
2020-02-19 19:13:08 +0530

    jupyter Notebook working on vs code

---
+++ ANACONDA/My First Image Processing Crash Course/standard_test_images/standard_test_images/untitled0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 18:54:34 2020
+
+@author: Kaustav
+"""
+
+import cv2
+
+def main():
+    print(cv2.__version__)
+    
+if __name__ == "__main__":
+    main()
