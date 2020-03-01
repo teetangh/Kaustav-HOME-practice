1feee3a81be81dd09681582db7ec70de444078db
Kaustav Ghosh <teetangh@gmail.com>
2020-03-01 23:19:26 +0530

    heapANDheapSort.c have doubts

--- C/Algo/Sorting Algorithms/heapANDheapSort.c
+++ C/Algo/Sorting Algorithms/heapANDheapSort.c
@@ -26,6 +26,17 @@ void maxHeapify(int *arr, int n, int i)
     }
 }
 
+void heapSort(int *arr, int n)
+{
+    for (int i = n / 2; i >= 1; i--)
+        maxHeapify(arr, n, i);
+    for (int i = n; i >= 1; i--)
+    {
+        swap(&arr[1], arr[i]);
+        maxHeapify(arr, n, 1);
+    }
+}
+
 int main()
 {
     int n;
@@ -38,9 +49,36 @@ int main()
     for (int i = 1; i <= n; i++)
         scanf(" %d", &arr[i]);
 
-    maxHeapify(arr, n, 1);
+    printf("Enter Your choice\n");
+    printf("1.maxHeapify\n");
+    printf("2.heapSort\n");
+    printf("3.Display Heap\n");
 
-    printf("\nThe array elements after maxHeapify are\n");
-    for (int i = 1; i <= n; i++)
-        printf(" %d", arr[i]);
+    int choice;
+    scanf(" %d", &choice);
+
+    while (1)
+    {
+        switch (choice)
+        {
+        case 1:
+            maxHeapify(arr, n, 1);
+            break;
+
+        case 2:
+            heapSort(arr, n);
+            break;
+        case 3:
+            printf("\nThe Heap elements are\n");
+            for (int i = 1; i <= n; i++)
+                printf(" %d", arr[i]);
+            break;
+
+        default:
+            exit(0);
+        }
+
+        printf("\nEnter Your choice\n");
+        scanf(" %d", &choice);
+    }
 }
