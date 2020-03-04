484d53ff858a68998239b7de8efa2348786d050d
Kaustav Ghosh <teetangh@gmail.com>
2020-03-04 21:14:58 +0530

    working in ipynb but not in py idkwhy

---
+++ Python/CSV Tutorials/Edureka/csvTutPandas.py
@@ -0,0 +1,12 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import pandas as pd
+
+result = pd.read_csv('trainTitanicKaggle.csv')
+print(result)
+
+
+# %%
+
+
