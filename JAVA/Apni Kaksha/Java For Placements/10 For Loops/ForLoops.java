48c14369feb4b2013aaf3cd4614339bf65eeb806
Kaustav Ghosh <teetangh@gmail.com>
2020-03-06 16:45:06 +0530

    added Java For Loops

--- JAVA/Apni Kaksha/Java For Placements/10 For Loops/ForLoops.java
+++ JAVA/Apni Kaksha/Java For Placements/10 For Loops/ForLoops.java
@@ -15,6 +15,24 @@ public class ForLoops {
         for (int i = 2; i < 100; i = i * i) {
             System.out.println("4th World " + i);
         }
+
+        int sum = 0;
+        int n = 100;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }j
+        System.out.println(sum);
+
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+
+        }
+
+        int tableOf = 4;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(tableOf * i);
+
+        }
     }
 
 }
