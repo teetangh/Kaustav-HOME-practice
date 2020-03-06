086498cbb7ceb341a4bcf6c63c994b151057c04a
Kaustav Ghosh <teetangh@gmail.com>
2020-03-06 10:12:44 +0530

    added BT

---
+++ Cpp/Cpp Nuts/Data Structures/Trees/prog3BTarrayReprsn.cpp
@@ -0,0 +1,11 @@
+// NOTES:
+// 0. Binary tree can be stored in array efficiently.
+
+// 1. if parent is p and 
+// Array starts from 0 to n-1 then
+//    left child = (2*p)+1
+//    right child = (2*p)+2
+
+// Array starts from 1 to n then
+//    left child = (2*p)
+//    right child = (2*p)+1
