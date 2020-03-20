7b1b5f4cb82449ea38065114369c95075bc4ff20
Kaustav Ghosh <teetangh@gmail.com>
2020-03-20 13:07:28 +0530

    trying a giraffe Academy's crash course in python

---
+++ Python/Crash Courses/Giraffe Academy Python Crash Course/app.py
@@ -0,0 +1,43 @@
+# Hello Wordl
+print('Hello World')
+
+# The Print Statement
+print()
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+print()
+
+
+print("There was onece a man nameed George.")
+print("He was 70 years old.")
+print("He really liked the name George.")
+print("But he didnt like being 70")
+print()
+
+# Variable and Data Types
+character_name = "John"
+character_age = 50.56
+is_Male = True
+print("There was onece a man named" + character_name + ",")
+print("He was " + str(character_age) + " years old.")
+print("He was Male:" + str(is_Male))
+character_name = "Mike"
+character_age = 45
+print("He really liked the name." + character_name)
+print("But he didnt like being " + str(character_age))
+print()
+
+
+# Strings in Python
+phrase = "Giraffe\'s Academy"
+print(phrase + "is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.lower().islower())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+
+
