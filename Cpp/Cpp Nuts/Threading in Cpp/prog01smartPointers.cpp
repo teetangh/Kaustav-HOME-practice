1e4ffab91d4981a191a3072bac3162437e618e85
Kaustav Ghosh <teetangh@gmail.com>
2020-04-30 03:09:58 +0530

    started unique pointers

--- Cpp/Cpp Nuts/Threading in Cpp/prog01smartPointers.cpp
+++ Cpp/Cpp Nuts/Threading in Cpp/prog01smartPointers.cpp
@@ -1,3 +1,12 @@
+// DESCRIPTION:
+// In this video we will learn what is the actual use of smart pointers in c++. 
+// It is basically a wrapper which wraps raw pointer and handles the life time of the object for you, means if there is no reference to the pointer it will delete it otherwise it will hold. There are basically three types of smart pointers. 
+// TYPES:
+// 1. unique_ptr
+// 2. shared_ptr
+// 3. weak_ptr
+// And the basic job of smart pointer is, it actually makes sure that we don't have memory leaks. 
+
 #include <iostream>
 using namespace std;
 
