9162446c2d9092345e0d950df7d5c6b058e70ecf
Kaustav Ghosh <teetangh@gmail.com>
2020-07-24 16:34:10 +0530

    learning a star

---
+++ Cpp/Cpp Knowledge Center - Abhishek Kumar/Smart Pointers/prog03_weak_pointer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <memory>
+
+struct A
+{
+    void printA()
+    {
+        std::cout << "A struct .." << std::endl;
+    };
+};
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
---
+++ Python/Game Dev/Tech With Tim/A_star.py
@@ -0,0 +1,271 @@
+import pygame
+import math
+from Queue import PriorityQueue
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("A* Path Findig Algorithm")
+
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGLE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+
+class Spot:
+
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row*width
+        self.y = col*width
+        self.color = WHITE
+        self.neighbours = []
+        self.width = width
+        self.total_rows = total_rows
+
+    def get_pos(self):
+        return self.row, self.col
+
+    def is_closed(self):
+        return self.color == RED
+
+    def is_open(self):
+        return self.color == GREEN
+
+    def is_barrier(self):
+        return self.color == BLACK
+
+    def is_start(self):
+        return self.color == ORANGLE
+
+    def is_end(self):
+        return self.color == TURQUOISE
+
+    def reset(self):
+        self.color = WHITE
+
+    def make_closed(self):
+        self.color = RED
+
+    def make_open(self):
+        self.color = GREEN
+
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_start(self):
+        self.color = ORANGLE
+
+    def make_end(self):
+        self.color = TURQUOISE
+
+    def make_path(self):
+        self.color = PURPLE
+
+    def draw(self, win):
+        pygame.draw.rect(
+            win, self.color, (self.x, self.y, self.width, self.width))
+
+    def update_neighbours(self, grid):
+        self.neighbours = []
+
+        # Can we move vertically down
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():
+            self.neighbours.append(grid[self.row + 1][self.col])
+
+        # Can we move vertically up
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():
+            self.neighbours.append(grid[self.row - 1][self.col])
+
+        # Can we move horizontally right
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():
+            self.neighbours.append(grid[self.row][self.col + 1])
+
+        # Can we move horizontally left
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col-1].is_barrier():
+            self.neighbours.append(grid[self.row][self.col-1])
+
+    # Less than
+    def __lt__(self, other):
+        return False
+
+
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+
+    # Keeps track of current shortest distance from start to this node
+    g_score = {spot: float("inf") for row in grid for spot in row}
+
+    # G Score from start to finish
+    g_score[start] = 0
+
+    # Keeps track of predicted distance via this path(node) to the end node
+    f_score = {spot: float("inf") for row in grid for spot in row}
+
+    # Initally heurisitic function from start to finish
+    f_score[start] = h(start.get_pos(), end.get_pos())
+
+    open_set_hash = {start}  # To see if something is in the open set
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        # getting only the node from [fscore , count , node]
+        current = open_set.get()[2]
+
+        # Synchronizing withe the open set hash (Removing the same node from the open set hash)
+        open_set_hash.remove(current)
+
+        # If we are at the end we done
+        if current == end:
+            return True
+    
+        # Or Else,keep finding
+        # Consider all the neighbours of the current node
+        for neighbor in current.neighbours:
+            # Calculate their temporary gscore
+            temp_g_score = g_score[current] + 1
+
+            # If we get a shorter gscore path , then update path
+            if temp_g_score < g_score[neighbor]:  
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + \
+                    h(neighbor.get_pos(), end.get_pos())
+
+                # Including the current nearest neighbour in the open set and syncing with open set hash
+                if neighbor not in open_set_hash:
+                    count += 1   
+                    open_set.put(f_score[neighbor], count, neighbor)
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+        draw()
+
+        if current != start:
+            current.make_closed()
+
+    return False
+
+
+def h(p1, p2):  # Heuristic Function(Using Manhattan Distance)
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows  # Integer Division
+
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):                   # Traversing across the rows
+            spot = Spot(i, j, gap, rows)        # Creating Spots row-wise
+            grid[i].append(spot)                # ith row
+
+    return grid
+
+
+def draw_grid(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i * gap),
+                         (width, i * gap))         # Horizontal lines
+        for j in range(rows):
+            pygame.draw.line(win, GREY, (j * gap, 0),
+                             (j * gap, width))     # Vertical lines
+
+
+def draw(win, grid, rows, width):
+    win.fill(WHITE)         # Painting entire screen white before each frames
+
+    for row in grid:
+        for spot in row:
+            spot.draw(win)
+
+    draw_grid(win, rows, width)
+    pygame.display.update()
+
+
+def get_clicked_position(pos, rows, width):
+    gap = width // rows
+    y, x = pos
+
+    row = y // gap
+    col = x // gap
+
+    return row, col
+
+
+def main(win, width):
+    ROWS = 50
+    grid = make_grid(ROWS, width)
+
+    start = None
+    end = None
+
+    run = True
+    started = False
+
+    while run:
+        draw(win, grid, ROWS, width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if started:
+                continue
+
+            if pygame.mouse.get_pressed()[0]:   # Left Mouse Button
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_position(pos, ROWS, width)
+                spot = grid[row][col]
+
+                if not start and spot != end:
+                    start = spot
+                    start.make_start()
+
+                elif not end and spot != start:
+                    end = spot
+                    end.make_end()
+
+                elif spot != end and spot != start:
+                    spot.make_barrier()
+
+            if pygame.mouse.get_pressed()[2]:   # Right Mouse Button
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_position(pos, ROWS, width)
+                spot = grid[row][col]
+                spot.reset()
+                if spot == start:
+                    start = None
+                elif spot == end:
+                    end = None
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started:
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbours(grid)
+
+                    # Passing a lamda function as parameter to the algorithm function
+                    algorithm(lambda: draw(win, grid, ROWS, width),
+                              grid, start, end)
+
+    pygame.quit()
+
+
+main(WIN, WIDTH)
